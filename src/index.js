a0fecd2d3263ef799e413a2067325545658b833d
Aleksandr Yemelianov <emelyanon@gmail.com>
2023-12-03 22:46:05 +0100

    delete basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
  
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/rental-cars">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
